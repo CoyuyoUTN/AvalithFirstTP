c3a10eb8a28e1cd8876696e1a1c4e3df60313c11
Gonzalo <herrero_gonza@hotmail.com>
2022-08-10 22:59:25 -0300

    :sparkles: delete innecesary code

--- js/cards.js
+++ js/cards.js
@@ -1,48 +1,6 @@
 document.addEventListener(`DOMContentLoaded`, () => {
-  /*let array = [];
-  for (let j = 1; j < 827; j++) {
-    array.push(j);
-  }
-
-  getInfo(array);*/
-
   pagination();
 });
-
-/*const getInfo = (array) => {
-  const fetchCards = axios
-    .get(`https://rickandmortyapi.com/api/character/${array}`)
-    .catch(function (error) {
-      if (error.response) {
-        // Request made and server responded
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-      }
-    });
-
-  fetchCards.then((response) => {
-    let body = ``;
-    const card = response.data;
-    console.log(card);
-    card.map((character) => {
-      body += `
-                <div class="card">
-                    <img class="image" src="${character.image}">
-                    <h2 class="id">     Id: ${character.id}</h2> 
-                    <h3 class="name"> Name: ${character.name}</h3>  
-                </div>
-            `;
-    });
-    document.getElementById("container").innerHTML = body;
-  });
-};*/
 
 const getInfo = (page) => {
   const fetchCards = axios

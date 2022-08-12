cb57498aa4b59008780a30766ab143652f5d9b1d
Gonzalo <herrero_gonza@hotmail.com>
2022-08-11 22:10:39 -0300

    :sparkles: new validation with js in form

--- js/form.js
+++ js/form.js
@@ -7,9 +7,41 @@ document.getElementById("send").addEventListener("click", function (evento) {
   const textarea = document.getElementById("textarea").value;
   console.log(`Usuario ${username} - Email ${email} - Coment ${textarea}`);
 
+  if (username.length == 0) {
+    alert("No has escrito nada en el usuario");
+    return;
+  }
+
+  const verification = validarEmail(email);
+  if (verification == 0) {
+    alert(
+      "La dirección de email es incorrecta!. Prueba con @hotmail.com o gmail.com"
+    );
+    return;
+  }
+
+  if (textarea.length == 0) {
+    alert("No has escrito nada en el mensaje");
+    return;
+  }
+
   localStorage.setItem("username", username);
   localStorage.setItem("email", email);
   localStorage.setItem("textarea", textarea);
 
   window.location.href = "../html/datForm.html";
 });
+
+const validarEmail = (email) => {
+  let bool = 0;
+  if (
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(
+      email
+    )
+  ) {
+    bool = 1;
+  } else {
+    bool = 0;
+  }
+  return bool;
+};
